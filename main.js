586150364d444800da5400d97f6c0fef25b29a24
Eddie Ewell III <85755488+edewell@users.noreply.github.com>
2021-08-02 14:51:41 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+function main() {
+ex1();
+ex2();
 
+}
+
+function ex1() {
+    let x1 = [11,12,13,14,15]
+    let x2 = [20.21,22,23,24]
+
+    // let (i, i < x2, x2.length, i++);
+    if ((x2 < x1) - 1);
+    return false;
+
+    
+}
+
+function ex2() {
+
+}
